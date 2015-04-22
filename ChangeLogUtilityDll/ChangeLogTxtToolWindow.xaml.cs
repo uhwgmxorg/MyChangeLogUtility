80eb7c97d133daf23c4fe0206ed843046ec2d014
Uwe on APOS-NBW10-02 <uhw@gmx.org>
2015-04-22 14:49:09 +0200

    Start.

---
+++ ChangeLogUtilityDll/ChangeLogTxtToolWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChangeLogUtilityDll
+{
+    /// <summary>
+    /// Interaktionslogik für ChangeLogTxtToolWindow.xaml
+    /// </summary>
+    public partial class ChangeLogTxtToolWindow : Window
+    {
+        public string BackgroundDisplayText { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ChangeLogTxtToolWindow(object parent)
+        {
+            InitializeComponent();
+            ((ContentControl)parent).Unloaded += new RoutedEventHandler(HaldelParentConsing);
+            BackgroundDisplayText = "ChangeLog";
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        /// <summary>
+        /// ParentConsing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void HaldelParentConsing(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        /// <summary>
+        /// ShowChangeLogWindow
+        /// </summary>
+        /// <param name="filename"></param>
+        public void ShowChangeLogWindow(string filename)
+        {
+            LableBackgroundDisplayText.Content = BackgroundDisplayText;
+            Paragraph Paragraph = new Paragraph(); 
+            try
+            {
+                Paragraph.Inlines.Add(System.IO.File.ReadAllText(filename));
+            }
+            catch (Exception)
+            {
+                Paragraph.Inlines.Add("File Not Found");
+            }
+            Paragraph.FontFamily = new FontFamily("Courier New");
+            Paragraph.FontSize = 12;
+            FlowDocument Document = new FlowDocument(Paragraph);
+            FlowDocReader.Document = Document;
+            FlowDocReader.ViewingMode = FlowDocumentReaderViewingMode.Scroll;
+            Show();
+        }
+
+        #endregion
+    }
+}
---
+++ MyChangeLogUtility/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using ChangeLogUtilityDll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyChangeLogUtility
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /******************************/
+        /*       Button Events        */
+        /******************************/
+        #region Button Events
+
+        /// <summary>
+        /// button1_Click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog Dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            Dlg.DefaultExt = ".txt";
+            Dlg.Filter = "Text documents (.txt)|*.txt";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> Result = Dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (Result == true)
+            {
+                // Open Document 
+                string Filename = Dlg.FileName;
+                FileNameTextBox.Text = Filename;
+                ChangeLogTxtToolWindow ChangeLogTxtToolWindow = new ChangeLogTxtToolWindow(this);
+                ChangeLogTxtToolWindow.ShowChangeLogWindow(Filename);
+            }
+        }
+
+        #endregion
+        /******************************/
+        /*      Menu Events          */
+        /******************************/
+        #region Menu Events
+
+        #endregion
+        /******************************/
+        /*      Other Events          */
+        /******************************/
+        #region Other Events
+
+        #endregion
+        /******************************/
+        /*      Other Functions       */
+        /******************************/
+        #region Other Functions
+
+        #endregion
+    }
+}
